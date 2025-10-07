81dc6f02efea82c717e91af47c122876af6d55df
Clarisse Carvalho <clarissemtcarvalho@gmail.com>
2025-10-07 15:57:31 +0200

    fix rad csv

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -11,20 +11,31 @@ from datetime import datetime
 import numpy as np
 import re
 import string
+import csv
 
 
 def read_data(file_location):
     dataset = []
     with open(file_location, 'r', encoding='utf-8') as infile:
-        for line in infile:
-            dataset.append(str(line.strip()).lower())
+        reader = csv.reader(infile)
+        next(reader)  
+        for row in reader:
+            user_id = int(row[0])
+            movie_id = int(row[1])
+            rating = float(row[2])
+            timestamp = int(row[3]) # ignore for now
+            dataset.append((user_id, movie_id, rating))
+    print(f"Read {len(dataset)} entries from {file_location}")
     return dataset
+
 
 def preprocess_data(dataset):
     print("Preprocessing data")
 
+
 def collaborative_filtering():
     print("Collaborative filtering algorithm")
+
 
 def finding_neighbors():
     print("Finding neighbors algorithm")
@@ -45,20 +56,20 @@ recommendations = [
         (2, 56, 4.2),
         (3, 45, 3.8),
         (4, 75, 2.8),
-        (5, 18, 1.4),
+        (5, 18, 4.9),
     ]
 
 
 
 def output(recommendations):
-    for user_id, movie_id, predicted_rating in recommendations:
-        print(f"{user_id} {movie_id} {predicted_rating:.2f}")
+    for pair_user_id, movie_id, predicted_rating in recommendations:
+        print(f"{pair_user_id} {movie_id} {predicted_rating:.2f}")
 
 def topN_output(n, recommendations):
-    print("top recommendations")
+    print("\nTop N recommendations")
     top = sorted(recommendations, key=lambda x: x[2], reverse=True)[:n]
     for user_id, movie_id, predicted_rating in top:
-        print(f"{user_id} {movie_id} {predicted_rating:.2f}")
+        print(f"{user_id} {movie_id} {predicted_rating}")
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:

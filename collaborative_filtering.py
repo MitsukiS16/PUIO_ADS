057c932c7fb9c4f051b8abd2f7bba23fee94e74b
Clarisse Carvalho <clarissemtcarvalho@gmail.com>
2025-10-07 11:09:44 +0200

    init project structure and which functions i need to wirte

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -20,16 +20,31 @@ def read_data(file_location):
             dataset.append(str(line.strip()).lower())
     return dataset
 
+def preprocess_data(dataset):
+    print("Preprocessing data")
 
+def collaborative_filtering():
+    print("Collaborative filtering algorithm")
 
+def finding_neighbors():
+    print("Finding neighbors algorithm")
+    print("first shingling")
+    print("then minhashing")
+    print("then LSH")
 
+def similarity_computation():
+    print("Similarity computation algorithm")
 
+def predict_ratings():
+    print("Predict ratings algorithm")
 
+def output():
+    print("top 5 recommendations")
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: python filtering.py <file_location> <similarity_threshold>")
+        print("Usage: python collaborative_filtering.py <file_location> <similarity_threshold>")
         sys.exit(1)
     file_location = sys.argv[1]
     dataset = read_data(file_location)
